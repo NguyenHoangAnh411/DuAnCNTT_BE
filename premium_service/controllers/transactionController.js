0668c7601e44b4d10cbfc305d0b695a929d772ea
NguyenHoangAnh411 <131466310+NguyenHoangAnh411@users.noreply.github.com>
2025-02-08 21:59:29 +0700

    update backend

--- premium_service/controllers/transactionController.js
+++ premium_service/controllers/transactionController.js
@@ -1,5 +1,33 @@
 const { database } = require('../../services/firebaseService');
 
 class TransactionController {
-    
+    static async getCompletedTransaction(req, res) {
+        try {
+            const completedTransactionRef = database.ref('completed_transactions');
+            const snapshot = await completedTransactionRef.once('value');
+            const data = snapshot.val() || {};
+
+            const result = Object.keys(data).map(key => ({ id: key, ...data[key] }))
+
+            return res.status(200).json(result);
+        } catch (e) {
+            res.status(500).json({ error: 'Failed to fetch Completed Transaction', details: error.message });
+        }
+    }
+
+    static async getPendingTransaction(req, res) {
+        try {
+            const pendingTransactionRef = database.ref('pending_transactions');
+            const snapshot = await pendingTransactionRef.once('value');
+            const data = snapshot.val() || {};
+
+            const result = Object.keys(data).map(key => ({ id: key, ...data[key] }))
+
+            return res.status(200).json(result);
+        } catch (e) {
+            res.status(500).json({ error: 'Failed to fetch Pending Transaction', details: error.message });
+        }
+    }
 }
+
+module.exports = TransactionController;
