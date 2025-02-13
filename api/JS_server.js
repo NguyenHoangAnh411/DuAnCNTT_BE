3b150e0f4ef817f6c57027d67eb7bd8d6db3612d
NguyenHoangAnh411 <131466310+NguyenHoangAnh411@users.noreply.github.com>
2025-02-13 21:13:56 +0700

    update back end

--- api/JS_server.js
+++ api/JS_server.js
@@ -7,17 +7,18 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/admin', require('./admin_service/server'));
-app.use('/chat', require('./chat_service/server'));
-app.use('/course', require('./course_service/server'));
-app.use('/exercise', require('./exercise_service/server'));
-app.use('/forum', require('./forum_service/server'));
-app.use('/games', require('./games_service/server'));
-app.use('/lesson', require('./lesson_service/server'));
-app.use('/personalized', require('./personalized_service/server'));
-app.use('/premium', require('./premium_service/server'));
-app.use('/user', require('./user_service/server'));
-app.use('/vocabulary', require('./vocabulary_service/server'));
+app.use('/', require('./admin_service/server'));
+app.use('/', require('./chat_service/server'));
+app.use('/', require('./course_service/server'));
+app.use('/', require('./exercise_service/server'));
+app.use('/', require('./forum_service/server'));
+app.use('/', require('./games_service/server'));
+app.use('/', require('./lesson_service/server'));
+app.use('/', require('./personalized_service/server'));
+app.use('/', require('./premium_service/server'));
+app.use('/', require('./user_service/server'));
+app.use('/', require('./vocabulary_service/server'));
+app.use('/', require('./chatbot_service/server'));
 
 app.get('/', (req, res) => {
   res.send('Welcome to DUANCNTT Backend!');
